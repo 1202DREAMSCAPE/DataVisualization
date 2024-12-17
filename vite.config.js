eea8250c75e4b4127f52a03138decd2dcf8d2b0a
Seajay <cjmcerezo2021@plm.edu.ph>
2024-12-18 07:15:57 +0800

    commentout

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import ApexCharts from 'apexcharts';
+//import ApexCharts from 'apexcharts';
 
 
 export default defineConfig({
